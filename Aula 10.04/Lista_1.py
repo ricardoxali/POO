5a61cdad9e84a1e052b85e7b8b468416a1984d8f
Ricardo <x.ricardo@academico.ifrn.edu.br>
2025-04-10 12:31:42 +0000

    Exercícios 1 e 2

--- Aula 10.04/Lista_1.py
+++ Aula 10.04/Lista_1.py
@@ -1,1 +1,10 @@
+# Exercício 1
+nome = 'Ricardo Xavier de Lima'
+matricula = '20241011110001'
+print(f"Nome: {nome} \nMatrícula: {matricula}")
 
+print()
+
+# Exercício 2
+nome = input("Digite seu primeiro nome:\n")
+print(f"Bem vindo(a) ao Codespaces, {nome}")
