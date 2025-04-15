e68578d8a7cb4419998f32428a357955a4020890
Ricardo <x.ricardo@academico.ifrn.edu.br>
2025-04-15 14:47:13 +0000

    lista 2 e um pouco da 3

--- Aula 15.04/Lista_2.py
+++ Aula 15.04/Lista_2.py
@@ -1,1 +1,30 @@
+#Exercício 1
+nome = input("Digite seu nome completo:\n")
+primeiro_nome = nome.split()[0]
+print(f'Bem vindo(a) ao Python, {primeiro_nome}')
 
+print()
+
+#Exercício 2
+nota1 = int(input("Digite a nota do primeiro bimestre da disciplina:\n"))
+nota2 = int(input("Digite a nota do segundo bimestre da disciplina:\n"))
+media = (nota1 * 2 + nota2 * 3) / 5
+print(f'Média parcial = {media:.0f}')
+
+print()
+
+#Exercício 3
+print('Digite a base e a altura do retângulo')
+base = int(input())
+altura = int(input())
+area = base * altura
+perimetro = 2 * base + 2 * altura
+diagonal = (base ** 2 + altura ** 2) ** (1/2)
+print(f'Área  = {area:.2f} - Perímetro = {perimetro:.2f} - Diagonal = {diagonal:.2f}')
+
+print()
+
+#Exercício 4
+frase = input('Digite uma frase:\n')
+ultima = frase[frase.rindex(' ') + 1:]
+print(ultima)
---
+++ Aula 15.04/Lista_3.py
@@ -0,0 +1,21 @@
+#Exercício 1004
+valor1 = int(input())
+valor2 = int(input())
+PROD = valor1 * valor2
+print(f'PROD = {PROD}')
+
+print()
+
+#Exercício 1005
+nota1 = float(input())
+nota2 = float(input())
+MEDIA = (nota1 * 3.5 + nota2 * 7.5) / 11
+print(f'MEDIA = {MEDIA:.5f}')
+
+print()
+
+#Exercício 1011
+raio = float(input())
+pi = 3.14159
+VOLUME = (4 / 3) * pi * raio ** 3
+print(f'VOLUME = {VOLUME:.3f}')
